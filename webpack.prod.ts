a26975f6b3975069a7eb4dbe0f03eee77d96aae7
mochammadramadhanjanuar <mochammadramadhanjanuarh@gmail.com>
2020-04-11 16:51:12 +0700

    fix

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,19 +1,13 @@
-import * as path from "path";
 import * as webpack from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+  template: "./src/index.html"
 });
 
 const config: webpack.Configuration = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
@@ -22,10 +16,31 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.css$/,
+        use: [
+          // style-loader
+          { loader: 'style-loader' },
+          // css-loader
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          // sass-loader
+          { 
+            loader: 'sass-loader', 
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin],
 };
 
 export default config;
